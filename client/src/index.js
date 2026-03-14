8b0d24135897e4ebb03ecd88c69cb2a2abfc2e60
Anvarjon <anvarqosimov153@gmail.com>
2026-03-14 20:32:01 +0500

    the site was finished

--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+// import { HashRouter } from "react-router-dom";
 import './i18n';
 import { LikeProvider } from './components/likedContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <LikeProvider>
         <App />
       </LikeProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
